33fa0c1185632c0a7774d287f1e79ab04ea48939
RevitalMarbel <revi85@gmail.com>
2018-03-02 02:00:45 +0200

    changed url of star file

--- src/StaticElements/videoFile.java
+++ src/StaticElements/videoFile.java
@@ -2,6 +2,6 @@ package StaticElements;
 
 public class videoFile {
 
-	public static String StarVideoName=" TLC00007.AVI";
+	public static String StarVideoName=" C:/Users/boaz/Desktop/starVideo/TLC00007.AVI";
 
 }
